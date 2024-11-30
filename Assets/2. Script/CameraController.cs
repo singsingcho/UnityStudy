132103e208a02b176d70bd3b9abd32ee80ad2551
singsingcho <83812901+singsingcho@users.noreply.github.com>
2024-11-30 11:06:33 +0900

    [기능] 카메라 팔로우
    - 캐릭터에 카메라를 자식오브젝트로 만들어서 캐릭터를 따라다니게 구현

--- Assets/2. Script/CameraController.cs
+++ Assets/2. Script/CameraController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+
     void Start()
     {
-        
+        offset = new Vector3(player.position.x, player.position.y + 4.0f, player.position.z + 10.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.position + offset;
+
+        transform.LookAt(player.position);
+
+        transform.RotateAround(player.transform.position, Vector3.up, Input.GetAxis("Mouse X") * turnSpeed);
     }
 }
