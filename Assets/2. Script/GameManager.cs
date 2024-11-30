adf6e0e82fdd1af7497f0e58572f8a3a3a6934d9
singsingcho <83812901+singsingcho@users.noreply.github.com>
2024-11-30 16:44:49 +0900

    [기능] 게임매니저 기능 수정
    - DonDestroyOnLoad선언 후 유저 이름, 플레이타임 정보 저장
    - PoolManager 게임 오브젝트 대리 호출

--- Assets/2. Script/GameManager.cs
+++ Assets/2. Script/GameManager.cs
@@ -3,20 +3,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // 싱글톤 인스턴스를 저장할 정적 변수
-    public static GameManager Instance { get; private set; }
-    [SerializeField] PoolManager poolManager;
+    public static GameManager Instance { get; private set; } = null;
+    public PlayManager playManager { get; private set; }
+    public PoolManager poolManager { get; private set; }
+
+    [field: SerializeField] public string userName { get; private set; }
+    [field: SerializeField] public float playTime { get; private set; }
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    public void SetUserName(string userName)
+    {
+        this.userName = userName;
+    }
+
+    public void UpdatePlayTime(float playTime)
+    {
+        this.playTime = playTime;
     }
 
     public GameObject GetObject(string key)
@@ -32,5 +47,10 @@ public class GameManager : MonoBehaviour
     public void ReturnObject(PoolManagedObject obj)
     {
         poolManager.ReturnObject(obj);
+    }
+
+    public void SetObjectPool(PoolManager manager)
+    {
+        this.poolManager = manager;
     }
 }
