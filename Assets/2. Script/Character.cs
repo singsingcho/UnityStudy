489f0e8c2be2aa4b1e8170a74f8a017e4302e511
singsingcho <83812901+singsingcho@users.noreply.github.com>
2024-11-30 11:09:41 +0900

    [기능] 캐릭터 이동, 점프, 상호작용 구현
    - InputSystem과 CharacterController를 이용해 wasd값을 받아 캐릭터 이동 구현
    - InputSystem을이용해 마우스 움직임 값을 받아 캐릭터 회전 구현
    - InputSystem과 CharacterController를 이용해 스페이스 값을 받아 캐릭터 점프 구현
    - InputSystem을 이용해 설정 범위 이내의 적 캐릭터의 이름 디버깅

--- Assets/2. Script/Character.cs
+++ Assets/2. Script/Character.cs
@@ -7,68 +7,107 @@ public class Character : MonoBehaviour
     [SerializeField] CharacterController controller;
 
     [SerializeField] InputActionProperty move;
+    [SerializeField] InputActionProperty look;
     [SerializeField] InputActionProperty jump;
+    [SerializeField] InputActionProperty interaction;
+
+    protected Vector2 MoveDirection;
+    protected Vector3 LookDirection;
 
     [SerializeField] float moveSpeed;
     [SerializeField] float rotateSpeed;
     [SerializeField] float jumpPower;
 
-    Vector3 moveDirection = Vector3.zero;
+    Vector3 controlDirection = Vector3.zero;
     private void Awake()
     {
-        move.reference.action.performed += Move;
-        jump.reference.action.performed += Jump;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Move(InputAction.CallbackContext obj)
+    private void Start()
     {
-        moveDirection = obj.ReadValue<Vector3>();
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            moveDirection.z = moveSpeed;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            moveDirection.x = -moveSpeed;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            moveDirection.z = -moveSpeed;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            moveDirection.x = moveSpeed;
-        }
-
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            moveDirection.z = 0;
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            moveDirection.x = 0;
-        }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            moveDirection.z = 0;
-        }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            moveDirection.x = 0;
-        }
-
-        controller.SimpleMove(moveDirection);
+        originalStepOffset = controller.stepOffset;
     }
 
-    void Jump(InputAction.CallbackContext obj)
+    private void FixedUpdate()
+    {
+        Move();
+        Look();
+        Jump();
+        Interaction();
+
+        controller.Move(controlDirection * Time.fixedDeltaTime);
+    }
+
+    void Move()
     {
         if (controller.isGrounded)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            MoveDirection = move.action.ReadValue<Vector2>();
+
+            controlDirection.x = MoveDirection.x * moveSpeed;
+            controlDirection.z = MoveDirection.y * moveSpeed;
+        }
+    }
+
+    void Look()
+    {
+        LookDirection = look.action.ReadValue<Vector2>();
+        LookDirection.y = 0f;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.forward + LookDirection), rotateSpeed * Time.deltaTime);
+    }
+
+    private float ySpeed;
+    private float originalStepOffset;
+    void Jump()
+    {
+        ySpeed += Physics.gravity.y * Time.fixedDeltaTime;
+
+        if (controller.isGrounded)
+        {
+            controller.stepOffset = originalStepOffset;
+            ySpeed = -0.8f;
+
+            if (jump.action.ReadValue<float>() > 0f)
             {
-                //controller.f
+                ySpeed = jumpPower;
             }
         }
+        else
+        {
+            controller.stepOffset = 0;
+        }
+
+        controlDirection.y = ySpeed;
+    }
+
+    void Interaction()
+    {
+        if (interaction.action.ReadValue<float>() > 0f)
+        {
+            GameObject[] go = GameObject.FindGameObjectsWithTag("Enemy");
+            for (int i = 0; i < go.Length; i++)
+            {
+                if (Vector3.Distance(go[i].transform.position, transform.position) < 15f)
+                {
+                    Debug.Log(go[i].name);
+                }
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        move.action.Enable();
+        look.action.Enable();
+        jump.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.action.Disable();
+        look.action.Disable();
+        jump.action.Disable();   
     }
 }
